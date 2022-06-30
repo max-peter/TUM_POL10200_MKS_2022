fbb63b68418df23be07780ffacfdf11d9c7ef8f7
Max Peter (iNUC) <max.peter@cironet.eu>
2022-06-30 19:37:47 +0200

    HW02

--- Hausaufgabe_2_0.R
+++ Hausaufgabe_2_0.R
@@ -1,17 +1,49 @@
 isRunningLocally = Sys.getenv("USERNAME") == "MaxPeter(cironeteu)"
 if(isRunningLocally) {
     # set working directory
-    
     # load library
     library(igraph)
     library(igraphdata)
     # load test data
-    txt = read.csv("Firma.txt", sep=";", encoding = "UTF-8")
-    # Den R data.frame in ein Netzwerkobjekt g umwandeln
-    g = graph_from_data_frame(txt, directed = F)
+    g1=read_graph("g1.txt", format =  "edgelist", directed=F)
+    g2=read_graph("g2.txt", format =  "edgelist", directed=F)
+    g3=read_graph("g3.txt", format =  "edgelist", directed=F)
+    g4=read_graph("g4.txt", format =  "edgelist", directed=F)
+    
 }
 
 if(isRunningLocally) {
-    # Test the function
-    plot (g) # Visualisierung des Netzwerkes
+    # Visualisierung des Netzwerkes
+    plot (g1)
+    plot (g2)
+    plot (g3)
+    plot (g4)
 }
+aufgabe_a<-function (g1, g2, g3, g4){
+
+  #Hier kommen Ihre Befehle
+  #....
+
+  return(c(tg1, tg2, tg3, tg4))
+
+}
+aufgabe_b<-function (g1,g2,g3,g4){
+
+  #Hier kommen Ihre Befehle
+  #....
+
+  return(list(dg1,dg2,dg3,dg4))
+
+}
+aufgabe_c<-function (g1,g2,g3,g4){
+
+  #Hier kommen Ihre Befehle
+  #....
+
+  return(c(hg1, hg2, hg3, hg4))
+
+}
+echtes_netzwerk='g1'
+erdos_renyi='g2'
+small_world='g3'
+barabasi_albert='g4'
