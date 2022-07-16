4c5b870d9a8efa62791da449cdfbf51fac6f667f
Max Peter (iNUC) <max.peter@cironet.eu>
2022-07-16 22:55:34 +0200

    Add Hausaufgaben Folder

---
+++ Hausaufgabe_4_x/Codekonstrukt_Simulation.R
@@ -0,0 +1,57 @@
+#### Ausgangssituation ######
+#infections = Status des Knotens 
+#(0=anfaellig, 1=infiziert, 2=immun)
+#tage = Tage seit Ansteckung des Knotens
+g=sample_smallworld(dim=1, luecke_a, nei=3, p=0.4)
+infections = rep(0,vcount(g))
+tage = rep(0,vcount(g))
+
+#Patient Null infizieren
+infect = sample(1:vcount(g),luecke_b)
+infections[infect] = luecke_c
+tage[infect] = luecke_d
+
+##### Simulation ######
+par(mfrow=c(4,4), mar=c(1,1,1,1))
+
+for (i in 1:luecke_e) {
+  print(i)
+  #Besuche alle infizierten Knoten
+  for (knoten in which(infections==luecke_f)){ 
+  
+    #Erhoehe die Anzahl an Tagen seit Ansteckung
+    tage[knoten]=tage[knoten]+1
+    print(tage[knoten])
+    
+    #Immun werden (nach .. Tagen)
+    if (tage[knoten]==luecke_g){
+      infections[knoten]=luecke_h
+    }
+    #oder Nachbarn infizieren
+    else {
+      #Speichere alle Nachbarn
+      neig=neighbors(g,knoten)
+      #Waehle die nicht-immunen Nachbarn aus
+      neig=neig[infections[neig]==0] 
+      #Stecke die nicht-immunen Nachbarn mit einer gewissen
+      #Wahrscheinlichkeit an
+      p=sample(1:100,length(neig),replace=T)
+      neig=neig[p<=luecke_i]
+      infections[neig] = luecke_j
+    }
+  }
+  #Besuche alle immunen Knoten
+  for (knoten in which(infections==luecke_k)){ 
+    #Erhoehe die Anzahl an Tagen seit Ansteckung
+    tage[knoten]=tage[knoten]+1
+    
+    #Immunisierung verlieren (falls .. Tage vergangen sind)
+    if (tage[knoten]==luecke_l){
+      infections[knoten]=0 #Infizierung zuruecksetzen
+      tage[knoten]=0 #Tage seit Ansteckung zuruecksetzen
+    }
+  }
+  #Plotte das Netzwerk
+  plot(g, vertex.color=infections, vertex.label=NA)
+}
+
